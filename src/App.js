59b05930a2c834a101c5db3e53ee151175a326f6
Kate Shaitarova <eshaytarova@gmail.com>
2018-06-09 00:00:32 +1200

    first commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import classes from './App.css';
+import Toolbar from './components/Navigation/Toolbar/Toolbar';
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <Toolbar/>
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/components/Logo/Logo.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import logo from './assets/images/logo.jpg';
+
+const logo = (props) => (
+    <div>
+        <img src={logo} alt="Logo" />
+    </div>
+);
+
+export default logo;
