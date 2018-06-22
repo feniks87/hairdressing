73421113fc549f4dbfdea4d3828b4dd92a293a3b
Kate Shaitarova <eshaytarova@gmail.com>
2018-06-22 16:06:00 +1200

    Filled ContactPage.js with content for 2 locations Changed Header padding

--- src/components/ContactPage/ContactPage.js
+++ src/components/ContactPage/ContactPage.js
@@ -1,10 +1,29 @@
 import React, { Component } from 'react';
+import Header from '../../components/UI/Header/Header';
+import BkLocation from '../../assets/images/location1.jpg';
+import LittleBkLocation from '../../assets/images/location2.jpg';
 
 
 class ContactPage extends Component {
     render() {
         return (
-            <div>
+            <div className="container">
+                <Header>Contact Us</Header>
+                <div className="row">
+                    <div className="col-sm-6">
+                        <h5 className="text-center">The BK Haidressing</h5>
+                        <p className="text-center">7/329 Albany Hwy, Rosedale, Auckland 0632</p>
+                        <p className="text-center">Call (09) 950-9994 for appointments!</p>
+                        <img className="img-fluid img-thumbnail" src={BkLocation} alt="BK Location"/>
+                    </div>
+                    <div className="col-sm-6">
+                        <h5 className="text-center">The Little BK Haidressing</h5>
+                        <p className="text-center">1 Raleigh Rd, Northcote,Auckland 0627</p>
+                        <p className="text-center">Call (09) 480-1390 for appointments!</p>
+                        <img className="img-fluid img-thumbnail" src={LittleBkLocation} alt="Little BK Location"/>
+                    </div>
+                </div>
+
             </div>
         )
     }
