ab8f2f43b0989bd81794d5e60ac0f5bc061837af
Kate Shaitarova <eshaytarova@gmail.com>
2018-06-24 20:48:42 +1200

    Added content for HomePage.js - About the salon and photos

--- src/components/HomePage/HomePage.js
+++ src/components/HomePage/HomePage.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 import ImageHair from '../../assets/images/1.jpg';
+import Salon from '../../assets/images/salon.jpg';
+import Haircut from '../../assets/images/haircut.jpg';
 import './HomePage.css';
 
 class HomePage extends Component {
@@ -7,7 +9,53 @@ class HomePage extends Component {
         return (
             <div>
                 <img className="Image img-fluid" src={ImageHair} alt="Hair"/>
+                <div className="container">
+                    <div className="row">
+                        <div className="col-sm-7">
+                            <h4 className="Paragraph-header">About The BK Hairdressing...</h4>
+                            <p className="text-justify">Some example text. Some example text. Some example text. Some example text. Some example text.
+
+                                Some example text. Some example text. Some example text. Some example text. Some example text.
+
+                                Some example text. Some example text. Some example text. Some example text. Some example text.
+
+                                Some example text. Some example text. Some example text. Some example text. Some example text.
+                                Some example text. Some example text. Some example text. Some example text. Some example text.
+
+                                Some example text. Some example text. Some example text. Some example text. Some example text.
+
+                                Some example text. Some example text. Some example text. Some example text. Some example text.
+
+                                Some example text. Some example text. Some example text. Some example text. Some example text.</p>
+
+                        </div>
+                        <div className="col-sm-5">
+                            <img className="Image-salon img-fluid" src={Salon} alt="Salon"/>
+                        </div>
+                    </div>
+                    <div className="row">
+                        <div className="col-sm-5">
+                            <img className="img-fluid" src={Haircut} alt="Haircut"/>
+                        </div>
+                        <div className="col-sm-7">
+                            <p className="text-justify">Some example text. Some example text. Some example text. Some example text. Some example text.
+
+                                Some example text. Some example text. Some example text. Some example text. Some example text.
+
+                                Some example text. Some example text. Some example text. Some example text. Some example text.
+
+                                Some example text. Some example text. Some example text. Some example text. Some example text.
+                                Some example text. Some example text. Some example text. Some example text. Some example text.
+
+                                Some example text. Some example text. Some example text. Some example text. Some example text.
+
+                                Some example text. Some example text. Some example text. Some example text. Some example text.</p>
+                        </div>
+                    </div>
+
+                    </div>
             </div>
+
         )
     }
 }
