b65dc57f92545baa014bba11b68ffcbb82178bc6
Kate Shaitarova <eshaytarova@gmail.com>
2018-07-14 00:21:54 +1200

    Add fetching data about stylists from API to OurTeamPage

--- src/components/pageComponents/OurTeamPage/OurTeamPage.js
+++ src/components/pageComponents/OurTeamPage/OurTeamPage.js
@@ -7,60 +7,42 @@ import Pic3 from '../../../assets/images/5.jpg'
 import Pic4 from '../../../assets/images/4.jpg'
 import TeamMember from '../../pageComponents/OurTeamPage/TeamMember/TeamMember';
 
+const PATH_STYLISTS = 'http://localhost:3000/api/stylists';
+
 class OurTeamPage extends Component {
 
+    constructor(props) {
+        super(props);
+        this.state = {
+            result: null,
+        };
+
+        this.setStylists = this.setStylists.bind(this);
+    }
+
+    setStylists(result) {
+        this.setState({ result });
+    }
+
+    componentDidMount() {
+        fetch(`${PATH_STYLISTS}`)
+            .then(response => response.json())
+            .then(result => this.setStylists(result))
+            .catch(error => console.log(error));
+    }
+
     render() {
+        const result = this.state.result;
+
+        console.log(result);
+        if (!result) { return null; }
+
         return (
            <div className="container">
                <Heading>Our Team</Heading>
-               <TeamMember src={Pic1} alt="Anna Stylist" name="Anna" description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam
-                   pulvinar risus non risus hendrerit venenatis. Pellentesque sit amet
-                   hendrerit risus, sed porttitor quam.
-                   Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam
-                   pulvinar risus non risus hendrerit venenatis. Pellentesque sit amet
-                   hendrerit risus, sed porttitor quam.
-                   Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam
-                   pulvinar risus non risus hendrerit venenatis. Pellentesque sit amet
-                   hendrerit risus, sed porttitor quam.
-                   Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam
-                   pulvinar risus non risus hendrerit venenatis. Pellentesque sit amet
-                   hendrerit risus, sed porttitor quam." />
-               <TeamMember src={Pic2} alt="Bella Stylist" name="Bella" description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam
-                   pulvinar risus non risus hendrerit venenatis. Pellentesque sit amet
-                   hendrerit risus, sed porttitor quam.
-                   Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam
-                   pulvinar risus non risus hendrerit venenatis. Pellentesque sit amet
-                   hendrerit risus, sed porttitor quam.
-                   Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam
-                   pulvinar risus non risus hendrerit venenatis. Pellentesque sit amet
-                   hendrerit risus, sed porttitor quam.
-                   Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam
-                   pulvinar risus non risus hendrerit venenatis. Pellentesque sit amet
-                   hendrerit risus, sed porttitor quam." />
-               <TeamMember src={Pic3} alt="Peter Stylist" name="Peter" description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam
-                   pulvinar risus non risus hendrerit venenatis. Pellentesque sit amet
-                   hendrerit risus, sed porttitor quam.
-                   Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam
-                   pulvinar risus non risus hendrerit venenatis. Pellentesque sit amet
-                   hendrerit risus, sed porttitor quam.
-                   Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam
-                   pulvinar risus non risus hendrerit venenatis. Pellentesque sit amet
-                   hendrerit risus, sed porttitor quam.
-                   Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam
-                   pulvinar risus non risus hendrerit venenatis. Pellentesque sit amet
-                   hendrerit risus, sed porttitor quam." />
-               <TeamMember src={Pic4} alt="Valentina Stylist" name="Valentina" description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam
-                   pulvinar risus non risus hendrerit venenatis. Pellentesque sit amet
-                   hendrerit risus, sed porttitor quam.
-                   Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam
-                   pulvinar risus non risus hendrerit venenatis. Pellentesque sit amet
-                   hendrerit risus, sed porttitor quam.
-                   Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam
-                   pulvinar risus non risus hendrerit venenatis. Pellentesque sit amet
-                   hendrerit risus, sed porttitor quam.
-                   Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam
-                   pulvinar risus non risus hendrerit venenatis. Pellentesque sit amet
-                   hendrerit risus, sed porttitor quam." />
+               {result.map(stylist =>
+                   <TeamMember src={Pic1} alt="Anna Stylist" name={stylist.name} description={stylist.description}/>
+               )}
 
            </div>
         )
