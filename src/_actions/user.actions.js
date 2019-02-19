2d4d940e295903e11ba2108ccfd1c3dc403dab9c
Kate Shaitarova <eshaytarova@gmail.com>
2019-02-20 11:19:22 +1300

    Change Loopback API endpoints to Firebase endpoints for actions related to a user
    Add Axios instead of Fetch for making HTTP requests
    Modify user actions - registering, logging in, logging out users, getting their info and updating it - connecting them to Firebase endpoints

--- src/_actions/user.actions.js
+++ src/_actions/user.actions.js
@@ -1,13 +1,13 @@
 import { userConstants } from '../_constants/user.constants';
-import { userService } from '../_services/user.service';
 import { alertActions } from './alert.actions';
+import axios from '../axios-instance';
 
 import { history } from '../_helpers/history';
 
 export const userActions = {
     login,
     logout,
-    register, 
+    register,
     getClient,
     updateClient,
 };
@@ -15,27 +15,29 @@ export const userActions = {
 function login(email, password) {
     return dispatch => {
         dispatch(request(email));
-
-        userService.login(email, password)
+        const url = 'https://www.googleapis.com/identitytoolkit/v3/relyingparty/verifyPassword?key=AIzaSyCfBr35UM1khoKnAl0G3JgwxJLujhKh_s8';
+        return axios.post(url, {email, password, returnSecureToken: true})
             .then(tokenInfo => {
                     history.push('/');
                     dispatch(success(tokenInfo));
-                    dispatch(userActions.getClient(tokenInfo.userId, tokenInfo.id));
-                },
-                error => {
+                    //dispatch(userActions.getClient(tokenInfo.data.localId, tokenInfo.id));In * 1000);
+                    const expirationDate = new Date(new Date().getTime() + tokenInfo.data.expiresIn * 1000);
+                    localStorage.setItem('token', tokenInfo.data.idToken);
+                    localStorage.setItem('expirationDate', expirationDate);
+                    localStorage.setItem('userId', tokenInfo.data.localId);
+                })
+            .catch(error => {
+                dispatch(failure(error.message));
+                dispatch(alertActions.error(error.message));
+                if (error.response.status === 401) {
+                    dispatch(failure(error.message));
+                    dispatch(alertActions.error("Email or Password is incorrect"));
+                }
+                else {
                     dispatch(failure(error.message));
                     dispatch(alertActions.error(error.message));
-
-                    if (error.response.status === 401) {
-                        dispatch(failure(error.message));
-                        dispatch(alertActions.error("Email or Password is incorrect"));
-                    }
-                    else {
-                        dispatch(failure(error.message));
-                        dispatch(alertActions.error(error.message));
-                    }
                 }
-            );
+            });
     };
 
     function request(email) { return { type: userConstants.LOGIN_REQUEST, email } }
@@ -43,9 +45,10 @@ function login(email, password) {
     function failure(error) { return { type: userConstants.LOGIN_FAILURE, error } }
 }
 
-function logout(id) {
-    userService.logout(id);
-    localStorage.removeItem('user');
+function logout() {
+    localStorage.removeItem('token');
+    localStorage.removeItem('expirationDate');
+    localStorage.removeItem('userId');
     history.push('/login');
     return {
         type: userConstants.LOGOUT
@@ -55,24 +58,45 @@ function logout(id) {
 function register(user) {
     return dispatch => {
         dispatch(request(user));
-
-        userService.register(user)
+        const url = 'https://www.googleapis.com/identitytoolkit/v3/relyingparty/signupNewUser?key=AIzaSyCfBr35UM1khoKnAl0G3JgwxJLujhKh_s8';
+        axios.post(url, user)
             .then(registration => {
-                    dispatch(success(registration));
-                    history.push('/login');
-                    dispatch(alertActions.success('You have been registered successfully!'));
-                },
-                error => {
-                    if (error.response.status === 422) {
-                        dispatch(failure(error.message));
-                        dispatch(alertActions.error("The email address you have entered is already registered"));
-                    }
-                    else {
-                        dispatch(failure(error.message));
-                        dispatch(alertActions.error(error.message));
-                    }
+                dispatch(success(registration));
+                history.push('/login');
+                dispatch(alertActions.success('You have been registered successfully!'));
+            })
+            .catch(error => {
+                debugger;
+                if (error.response.status === 400 && error.response.data.error.message === "EMAIL_EXISTS") {
+                    debugger;
+                    dispatch(failure(error.message));
+                    dispatch(alertActions.error("The email address you have entered is already registered"));
                 }
-            );
+                else {
+                    dispatch(failure(error.message));
+                    dispatch(alertActions.error(error.message));
+                }
+            });
+
+
+        axios.post('/users.json', user)
+            .then(registration => {
+                dispatch(success(registration));
+                history.push('/login');
+                dispatch(alertActions.success('You have been registered successfully!'));
+            })
+            .catch(error => {
+                debugger;
+                if (error.response.status === 400 && error.response.data.error.message === "EMAIL_EXISTS") {
+                    debugger;
+                    dispatch(failure(error.message));
+                    dispatch(alertActions.error("The email address you have entered is already registered"));
+                }
+                else {
+                    dispatch(failure(error.message));
+                    dispatch(alertActions.error(error.message));
+                }
+            });
     };
 
     function request(user) { return { type: userConstants.REGISTER_REQUEST, user } }
@@ -83,15 +107,14 @@ function register(user) {
 function getClient(clientId, token) {
     return dispatch => {
         dispatch(request(clientId));
-
-        userService.getClient(clientId, token)
+        axios.get('/users.json', clientId, token)
             .then(clientInfo => {
-                    dispatch(success(clientInfo));
-                },
-                error => {
-                    dispatch(failure(error.message));
-                    dispatch(alertActions.error(error.message));
-                }
+                dispatch(success(clientInfo));
+            },
+            error => {
+                dispatch(failure(error.message));
+                dispatch(alertActions.error(error.message));
+            }
             );
     };
 
@@ -104,7 +127,7 @@ function updateClient(client, token) {
     return dispatch => {
         dispatch(request(client.id));
 
-        userService.updateClient(client, token)
+        axios.patch('/users.json', client, token)
             .then(clientInfo => {
                     dispatch(success(clientInfo));
                     dispatch(alertActions.success('Details were successfully updated'));
